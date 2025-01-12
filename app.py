96db5b2b4340610d9ee67b78690330ba3902d62c
Fahad <fahadyousuf344@gmail.com>
2025-01-12 17:24:51 +0500

    Port fixed

--- app.py
+++ app.py
@@ -808,5 +808,5 @@ if __name__ == '__main__':
     start_stock_threads()
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
